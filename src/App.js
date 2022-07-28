c3798a3fe56c2e93a62c423afaf22e4f4ff872aa
Mazdul1000 <mazdul1000@gmail.com>
2022-07-28 21:12:20 +0600

    created redux store and setup authentication routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import {
+BrowserRouter as Router,
+Routes,
+Navigate,
+Route
+}from 'react-router-dom';
+import LoginPage from './authPages/LoginPage/LoginPage';
+import RegisterPage from './authPages/RegisterPage/RegisterPage';
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+   <>
+   <Router>
+   <Routes>
+      <Route exact path='/login' element={<LoginPage></LoginPage>}>
+      </Route>
+      <Route exact path='/register' element={<RegisterPage></RegisterPage>}>
+      </Route>
+      <Route exact path='/dashboard' element={<Dashboard></Dashboard>}>   
+      </Route>
+      <Route path='/' element={<Dashboard></Dashboard>}>  
+      </Route>
+    </Routes>
+   </Router>
+   </>
   );
 }
 
